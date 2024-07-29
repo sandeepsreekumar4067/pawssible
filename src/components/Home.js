3f636575180bae1e6a98e1413a399412b5a35d04
sandeepsreekumar4067 <sandeepsreekumar4067@gmail.com>
2024-07-29 21:02:41 +0530

    success fetching data from firestore

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,36 @@
+import {
+    collection,
+    getDocs
+} from 'firebase/firestore';
 import '../style/home.css'
+import {
+    db
+} from './firebaseAuth';
+import {
+    useEffect,
+    useState
+} from 'react';
 const Home = () => {
-    return ( 
-    <div className="home-container">
-        
-    </div> );
-}
- 
-export default Home;
+        const [petData, setPetData] = useState()
+        const fetchPetDetails = async () => {
+            const petref = collection(db, 'Pet Details')
+            const snapShot = await getDocs(petref)
+            const fetchedData = snapShot.docs.map((doc) => ({
+                id: doc.id,
+                ...doc.data()
+            }))
+            setPetData(await fetchedData)
+        }
+        // useEffect(() => {
+        //     fetchPetDetails();
+        // }, []);
+
+        // useEffect(() => {
+        //     console.log(petData);
+        // }, [petData]);
+        return ( <div className = "home-container" >
+            
+        </div> );
+        }
+
+        export default Home;
